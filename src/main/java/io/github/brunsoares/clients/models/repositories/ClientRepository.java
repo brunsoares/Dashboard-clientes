e5d01680dee07fed9cc46f025ee7166071a7d746
brunsoares <brunohenrique.soares@outlook.com>
2022-09-01 22:30:53 -0300

    API-04 #comment
    Alterações Efetuadas:
    - Alteração de diretorio, para organização do projeto backend com frontend;

--- src/main/java/io/github/brunsoares/clients/models/repositories/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.brunsoares.clients.models.repositories;
-
-import io.github.brunsoares.clients.models.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-
-}
--- src/main/java/io/github/brunsoares/clients/models/repositories/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.brunsoares.clients.models.repositories;
-
-import io.github.brunsoares.clients.models.entities.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Integer> {
-
-}
