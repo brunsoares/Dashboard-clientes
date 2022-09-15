3149c2b2056549ea8b3cba14f1caa761a5b729c0
brunsoares <brunohenrique.soares@outlook.com>
2022-09-14 22:13:53 -0300

    DASHBOARD-03 #comment
    Alterações Efetuadas:
    - Criação das rotas (home, client);
    - Vincular rotas aos componentes da sidebar;
    - Criação do front da tela cadastro clientes, com tratativas nos campos;
    - Tratativas responsivas;

---
+++ clientes-dashboard/src/app/clients/clients-form/clients-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Client } from '../clients';
+
+@Component({
+  selector: 'app-clients-form',
+  templateUrl: './clients-form.component.html',
+  styleUrls: ['./clients-form.component.css']
+})
+export class ClientsFormComponent implements OnInit {
+
+  client: Client;
+
+  constructor() {
+    this.client = new Client();
+    
+   }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ clientes-dashboard/src/app/clients/clients-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ClientsFormComponent } from './clients-form/clients-form.component';
+
+
+const routes: Routes = [
+  { path: 'clients', component:ClientsFormComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ClientsRoutingModule { }
