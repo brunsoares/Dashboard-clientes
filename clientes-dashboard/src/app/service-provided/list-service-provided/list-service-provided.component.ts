4c80c0737723850cd62060e1cc3268c46ac17a80
brunsoares <brunohenrique.soares@outlook.com>
2022-09-22 23:28:51 -0300

    DASHBOARD-09 #comment
    Alterações Efetuadas:
    - Ajuste campo de mês;
    - Criado função para trazer mês descritivo;

--- clientes-dashboard/src/app/service-provided/list-service-provided/list-service-provided.component.ts
+++ clientes-dashboard/src/app/service-provided/list-service-provided/list-service-provided.component.ts
@@ -10,6 +10,7 @@ export class ListServiceProvidedComponent implements OnInit {
   name: string;
   month: number;
   months: number[];
+  monthDescribed: String;
 
   constructor() { 
     this.months = [1,2,3,4,5,6,7,8,9,10,11,12];
@@ -19,7 +20,51 @@ export class ListServiceProvidedComponent implements OnInit {
   }
 
   onSubmit(){
-    
+
+  }
+  
+  setMonthDescribed(mouthNumber: number){
+    switch (mouthNumber){
+      case 1:
+        this.monthDescribed = "Janeiro";
+        break;
+      case 2:
+        this.monthDescribed = "Fevereiro";
+        break;
+      case 3:
+        this.monthDescribed = "Março";
+        break;
+      case 4:
+        this.monthDescribed = "Abril";
+        break;
+      case 5:
+        this.monthDescribed = "Maio";
+        break;
+      case 6:
+        this.monthDescribed = "Junho";
+        break;
+      case 7:
+        this.monthDescribed = "Julho";
+        break;
+      case 8:
+        this.monthDescribed = "Agosto";
+        break;
+      case 9:
+        this.monthDescribed = "Setembro";
+        break;
+      case 10:
+        this.monthDescribed = "Outubro";
+        break;
+      case 11:
+        this.monthDescribed = "Novembro";
+        break;
+      case 12:
+        this.monthDescribed = "Dezembro";
+        break;
+      default:
+        this.monthDescribed = "";
+        break;
+    }
   }
 
 }
